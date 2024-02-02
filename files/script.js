2187116a9cf8785feaa227ed99e92e0be9eb835a
isaac-diniz <dinizisaac266@gmail.com>
2024-02-01 21:59:46 -0300

    Portifolio pessoal - finalizado

---
+++ files/script.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".multiple-text", {
+    strings: ["Designer", "BackEnd Developer in Training"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+})
